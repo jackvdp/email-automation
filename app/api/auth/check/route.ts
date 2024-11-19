eb595ecf47c1fc7c8405fbbee39022d95d3d8596
jackvdp <jack@pumpymusic.co.uk>
2024-11-19 16:13:42 +0000

    Log token and id

--- app/api/auth/check/route.ts
+++ app/api/auth/check/route.ts
@@ -1,11 +1,19 @@
 // app/api/auth/check/route.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
+import sessionStore from '@/utils/sessionStore';
 
 export async function GET(req: NextRequest) {
     const sessionToken = cookies().get('graph-session')?.value;
+    var sessionID: string | undefined;
+
+    if (sessionToken) {
+        sessionID = sessionStore.get(sessionToken);
+    }
+
+    console.log(`***** Session token: ${sessionToken}, Session ID: ${sessionID}`);
 
     return NextResponse.json({
-        isAuthenticated: !!sessionToken
+        isAuthenticated: !!sessionToken,
     });
 }

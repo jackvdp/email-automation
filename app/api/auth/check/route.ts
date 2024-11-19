e836b015b6b270c7f30a4e50f4d7df9753f0f74b
jackvdp <jack@pumpymusic.co.uk>
2024-11-19 17:07:40 +0000

    Add extra check for check api

--- app/api/auth/check/route.ts
+++ app/api/auth/check/route.ts
@@ -14,6 +14,6 @@ export async function GET(req: NextRequest) {
     console.log(`***** Session token: ${sessionToken}, Session ID: ${sessionID}`);
 
     return NextResponse.json({
-        isAuthenticated: !!sessionToken,
+        isAuthenticated: !!sessionToken && !!sessionID,
     });
 }
